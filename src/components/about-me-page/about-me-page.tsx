65a3d18b2853de758c5ad5c4f5ab5eacce43fdf2
Dylan Winters <djpwinters2002@gmail.com>
2025-07-27 21:38:37 +0100

    Moves all components to the home page

--- src/components/about-me-page/about-me-page.tsx
+++ src/components/about-me-page/about-me-page.tsx
@@ -3,7 +3,7 @@ import "./about-me-page.scss";
 
 export const AboutMePage = () => {
   return (
-    <div className="about-me-page">
+    <div id="about-me-section" className="container-fluid text-center section--height bg-light">
       <h1>About Me</h1>
       <p>This is the about me page content.</p>
     </div>
--- src/components/home-page/home-page.tsx
+++ src/components/home-page/home-page.tsx
@@ -1,13 +1,25 @@
 import React from 'react';
 import './home-page.scss';
+import { AboutMePage } from '../about-me-page/about-me-page';
+import { ProjectsPage } from '../projects-page/projects-page';
+
+const HomePageIntroduction = () => {
+  return (
+    <div id="home" className="home-page container-fluid text-center section--height">
+      <h1>Welcome to my Portfolio</h1>
+      <p>This is the home page content.</p>
+    </div>
+  )
+}
 
 export const HomePage = () => {
 
 
   return (
-    <div className="home-page">
-      <h1>Welcome to My Portfolio</h1>
-      <p>This is the home page content.</p>
+    <div className='container d-flex flex-column home-page--height'>
+      <HomePageIntroduction />
+      <AboutMePage />
+      <ProjectsPage isOverview={true} projectLimit={1} />
     </div>
   );
 };
