a1b4dc858a193540b63150ecc66cbcbd31757b95
Dylan Winters <djpwinters2002@gmail.com>
2026-02-13 17:55:34 +0000

    Add dev and prod api url env vars

--- src/config/setupAxiosInterceptors.ts
+++ src/config/setupAxiosInterceptors.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-axios.defaults.baseURL = import.meta.env.BASE_URL ?? "http://localhost:8080";
+axios.defaults.baseURL =
+  import.meta.env.VITE_API_URL || "http://localhost:8080";
 
 const setupAxiosInterceptors = () => {};
 
